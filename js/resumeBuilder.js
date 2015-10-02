2adc17efc1482f4c0c73942dc0482a0d8d63d1b6
JP Lacy <lacyjpr@gmail.com>
2015-10-02 07:56:17 -0700

    feat: Add if statement and for loop

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,32 +9,34 @@ var bio = {
 		"location": "Newport, OR"
 	},
 	"welcomeMessage": "Problem Solver ",
-	"skills": ["HTML, ", "CSS, ", "Managing, ", "Coaching, ", "Teaching, ", "Mentoring "],
+	"skills": ["HTML", "CSS", "Managing", "Coaching", "Teaching", "Mentoring", "Customer Service"],
 	"bioPic": "images/JPLacy.jpg"
 }
-//$("#main").append(bio.name, bio.role, bio.contacts, bio.welcomeMessage, bio.skills, bio.bioPic);
-// if(bio.skills.length > 0) {
-// 	$("#header").append(HTMLskillsStart);
-// 	var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-// 	$("#skills").append(formattedSkill);
-// 	formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
-// 	$("#skills").append(formattedSkill);
-// 	formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
-// 	$("#skills").append(formattedSkill);
-// 	formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
-// 	$("#skills").append(formattedSkill);
-// 	formattedSkill = HTMLskills.replace("%data%", bio.skills[4]);
-// 	$("#skills").append(formattedSkill);
-// 	formattedSkill = HTMLskills.replace("%data%", bio.skills[5]);
-// 	$("#skills").append(formattedSkill);
-// }
+
+if(bio.skills.length > 0) {
+	$("#header").append(HTMLskillsStart);
+	var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[4]);
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[5]);
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[6]);
+	$("#skills").append(formattedSkill);
+}
 
 var work = {
 	"jobs": [
 	{
 		"employer": "City of Newport",
 		"title": "Control Desk Clerk, Newport Recreation Center",
-		"location": "Newport, Or",
+		"location": "Newport, OR",
 		"dates": "May 2014 - Current",
 		"description": "Customer Service. Greet customers and meet their needs as necessary. Manage a cash register. Membership and pass sales. Room reservations and scheduling."
 	},
@@ -50,7 +52,7 @@ var work = {
 		"title": "Interim Director of Debate",
 		"location": "Winston-Salem, NC",
 		"dates": "July 2009-June 2010",
-		"description": "Direct, oversee, budget, administer and manage Wake Forest Debate. Responsible for a team of 20 debaters seven assistant coaches and a travel budget of $120,000."
+		"description": "Direct, oversee, budget, administer and manage Wake Forest Debate. Responsible for a team of 20 debaters, seven assistant coaches, event hosting, and a travel budget of $120,000."
 	},
 	{
 		"employer": "Whitman College",
@@ -63,11 +65,29 @@ var work = {
 		"employer": "Lakeside High School",
 		"title": "Social Studies Teacher, Debate Coach",
 		"location": "Augusta, GA",
-		"dates": "August 1995-June1996",
+		"dates": "August 1995-June 1996",
 		"description": "Teach US History, World History and Debate. Administer coach and travel the Lakeside High School Debate Team"
 	}
   ]
 }
+
+for(job in work.jobs) {
+	$("#workExperience").append(HTMLworkStart);
+	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+	var formattedEmployerTitle = formattedEmployer + formattedTitle;
+	$(".work-entry:last").append(formattedEmployerTitle);
+
+	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+	$(".work-entry:last").append(formattedDates);
+
+	var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+	$(".work-entry:last").append(formattedLocation);
+
+	var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+	$(".work-entry:last").append(formattedDescription);
+}
+
 
 var education = {
 	"schools": [
@@ -84,7 +104,7 @@ var education = {
 			"location": "Winston-Salem, NC, US",
 			"degree": "Graduate Study Towards MA",
 			"majors": ["Communication"],
-			"dates": ,
+			"dates": 1994,
 			"url": "http://www.wfu.edu/"
 		}
 	],
@@ -101,18 +121,19 @@ var education = {
 var projects = {
 	"projects": [
 		{
-			"title": "Introduction to Programming Final Project"
-			"dates": "2015"
-			"description": "Final project for Udacity's Introduction to Programming Nanodegree. Webapp hosted by Google App Engine. Includes my notes for the course, a comments section, and JavaScript for collapsible navigation."
+			"title": "Introduction to Programming Final Project",
+			"dates": "2015",
+			"description": "Final project for Udacity's Introduction to Programming Nanodegree. Webapp hosted by Google App Engine. Includes my notes for the course, a comments section, and JavaScript for collapsible navigation.",
 			"images": ["images/ipnd1.jpg", "images/ipnd2.jpg"]
 		},
 		{
-			"title": "Portfolio"
-			"dates": "2015"
-			"description": "First project for Udacity's Front-End Web Developer Nanodegree. We were given a pdf design mockup to replicate. Includes responsive images and interactive modals."
+			"title": "Portfolio",
+			"dates": "2015",
+			"description": "First project for Udacity's Front-End Web Developer Nanodegree. We were given a pdf design mockup to replicate and customize. Includes customized responsive images and interactive modals.",
 			"images": ["images/portfolio1.jpg", "images/portfolio2.jpg"]
 		}
 	]
+}
 
 
 // $(document).click(function(loc) {
